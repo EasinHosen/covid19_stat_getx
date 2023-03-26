d29fb211d0e270c965a1533b0d331b4940d9342e
EasinHosen <easinhosen5@gmail.com>
2023-03-26 16:31:42 +0600

    country details page implemented

--- lib/pages/country_details_page.dart
+++ lib/pages/country_details_page.dart
@@ -3,6 +3,9 @@ import 'package:covid19_stat/models/country_stat_model.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../utils/helper_functions.dart';
+import '../widgets/custom_data_row.dart';
+
 class CountryDetailsPage extends StatelessWidget {
   CountryDetailsPage({Key? key}) : super(key: key);
 
@@ -13,6 +16,77 @@ class CountryDetailsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(countryStatModel.country!),
         centerTitle: true,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Center(
+                  child: Image(
+                    width: Get.width * 0.5,
+                    height: Get.height * 0.2,
+                    image: NetworkImage(countryStatModel.countryInfo!.flag!),
+                  ),
+                ),
+                Card(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      children: [
+                        const Text('Last updated'),
+                        Text(
+                          getFormattedDateTime(
+                            DateTime.fromMillisecondsSinceEpoch(
+                                countryStatModel.updated!.toInt()),
+                            'dd-MMM-yyyy \n hh:mm a',
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        CustomDataRow(
+                          title: 'Total tests',
+                          data: countryStatModel.tests.toString(),
+                        ),
+                        CustomDataRow(
+                          title: 'Total case',
+                          data: countryStatModel.cases.toString(),
+                        ),
+                        CustomDataRow(
+                            title: 'Total Recovered',
+                            data: countryStatModel.recovered.toString()),
+                        CustomDataRow(
+                            title: 'Total Dead',
+                            data: countryStatModel.deaths!.toString()),
+                        const Divider(),
+                        CustomDataRow(
+                            title: 'Today\'s case',
+                            data: countryStatModel.todayCases.toString()),
+                        CustomDataRow(
+                            title: 'Today\'s recovered',
+                            data: countryStatModel.todayRecovered.toString()),
+                        CustomDataRow(
+                            title: 'Today\'s death',
+                            data: countryStatModel.todayDeaths.toString()),
+                        const Divider(),
+                        CustomDataRow(
+                            title: 'Active cases',
+                            data: countryStatModel.active.toString()),
+                        CustomDataRow(
+                            title: 'Critical cases',
+                            data: countryStatModel.critical.toString()),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
